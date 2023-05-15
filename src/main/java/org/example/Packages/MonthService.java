cab038684e9ded1e4fcba729dfc80ae3b2c10f36
Oskina Margarita <rita_2113@mail.ru>
2023-05-16 01:44:25 +0300

    Added works

---
+++ src/main/java/org/example/Packages/MonthService.java
@@ -0,0 +1,20 @@
+package org.example.Packages;
+
+ class MonthService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счетчик месяцев отдыха
+        int money = 0; // количество денег на счету
+
+
+        for (int month =0; month <12; month++) {
+            if (money >= threshold) {
+                count++;
+                money = (money - expenses)/3;
+            }else {
+                money = money + income - expenses;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/org/example/Packages/MonthServiceTest.java
@@ -0,0 +1,19 @@
+package org.example.Packages;
+// import org.example.Packages.MonthService;
+import org.junit.jupiter.api.Test;
+
+public class MonthServiceTest {
+
+    @Test
+
+    void CalculateHolidays(int count) {
+        MonthService service = new MonthService;
+
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        System.out.println(count);
+
+    }
+}
